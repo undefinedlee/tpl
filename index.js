5aed81a4daaf0fc0ef5aad5b339f68115846ba1d
super <0@00010101.com>
2016-01-06 17:10:08 +0800

    ...

---
+++ index.js
@@ -0,0 +1,15 @@
+function tpl(template) {
+	try {
+		return new Function("Model", "var _$_ = [],model=Model;" + ("$>" + template.trim() + "<$").replace(/<\$\s*=\s*([\s\S]*?)\s*\$>/g, function (a, b) {
+			return "<$ _$_.push(" + b + "); $>";
+		}).replace(/\$>([\s\S]*?)<\$/g, function (a, b) {
+			if (/^\s*$/.test(b))
+				return "";
+			else
+				return "_$_.push('" + b.replace(/\\/g, "\\\\").replace(/\r\t\n/g, " ").replace(/'/g, "\\'") + "');";
+		}) + "return _$_.join('');");
+	} catch (e) {
+		console.error(e);
+		return function(){}
+	}
+}
