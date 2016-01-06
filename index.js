73eb3f0429168e627e8c08265940ebb77dee5d7e
super <0@00010101.com>
2016-01-06 17:35:49 +0800

    ...

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function tpl(template) {
 	try {
-		return new Function("Model", "var _$_ = [],model=Model;" + ("$>" + template.trim() + "<$").replace(/<\$\s*=\s*([\s\S]*?)\s*\$>/g, function (a, b) {
+		return new Function("Model", "var _$_ = [],model=Model;" + ("$>" + template + "<$").replace(/<\$\s*=\s*([\s\S]*?)\s*\$>/g, function (a, b) {
 			return "<$ _$_.push(" + b + "); $>";
 		}).replace(/\$>([\s\S]*?)<\$/g, function (a, b) {
-			if (/^\s*$/.test(b))
-				return "";
-			else
-				return "_$_.push('" + b.replace(/\\/g, "\\\\").replace(/[\r\t\n]/g, " ").replace(/'/g, "\\'") + "');";
+			return "_$_.push('" + b.replace(/\\/g, "\\\\").replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/'/g, "\\'") + "');";
 		}) + "return _$_.join('');");
 	} catch (e) {
 		console.error(e);
